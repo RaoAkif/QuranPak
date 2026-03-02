c50614a7be753449bc3df448e2482b6e678bb980
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2026-03-02 17:36:58 +0500

    Add Only Arabic Surah Data

---
+++ paraData.js
@@ -0,0 +1,153 @@
+export const paraData = {
+    1: {
+      name: 'الم',
+      en_name: 'Alif-laam-meem',
+      link: 'https://www.youtube.com/watch?v=TT82Eh5TCjM',
+    },
+    2: {
+      name: 'سَيَقُولُ',
+      en_name: 'Sayaqūlu',
+      link: 'https://www.youtube.com/watch?v=egSuM-eNBMQ',
+    },
+    3: {
+      name: 'الرُّسُلُ تِلْكَ',
+      en_name: 'Tilka ’r-Rusulu',
+      link: 'https://www.youtube.com/watch?v=nJXwXjHpdgw',
+    },
+    4: {
+      name: 'تَنَالُوا لَنْ',
+      en_name: 'Lan Tana Loo',
+      link: 'https://www.youtube.com/watch?v=W6eTUuFvMto',
+    },
+    5: {
+      name: 'وَالْمُحْصَنَاتُ',
+      en_name: 'Wal Mohsanat',
+      link: 'https://www.youtube.com/watch?v=iK21td3SdzA',
+    },
+    6: {
+      name: 'اللَّهُ يُحِبُّ لَا',
+      en_name: 'La Yuhibbullah',
+      link: 'https://www.youtube.com/watch?v=8GEzyORAltU',
+    },
+    7: {
+      name: 'سَمِعُوا وَإِذَا',
+      en_name: 'Wa Iza Samiu',
+      link: 'https://www.youtube.com/watch?v=ic4zSk9XUpg',
+    },
+    8: {
+      name: 'أَنَّنَا وَلَوْ',
+      en_name: 'Wa Lau Annana',
+      link: 'https://www.youtube.com/watch?v=86tPQHPQ4EI',
+    },
+    9: {
+      name: 'الْمَلَأُ قَالَ',
+      en_name: 'Qalal Malao',
+      link: 'https://www.youtube.com/watch?v=Ugn8vjRJC5Y',
+    },
+    10: {
+      name: 'وَاعْلَمُوا',
+      en_name: 'Wa A\'lamu',
+      link: 'https://www.youtube.com/watch?v=op02RaFl1qc',
+    },
+    11: {
+      name: 'يَعْتَذِرُونَ',
+      en_name: 'Yatazeroon',
+      link: 'https://www.youtube.com/watch?v=DN05Ud3XWUg',
+    },
+    12: {
+      name: 'دَابَّةٍ مِنْ وَمَا',
+      en_name: 'Wa Mamin Da\'abat',
+      link: 'https://www.youtube.com/watch?v=aXaJVv6_GRw',
+    },
+    13: {
+      name: 'بَرِّئُوَمَا',
+      en_name: 'Wa Ma Ubrioo',
+      link: 'https://www.youtube.com/watch?v=ACFOcvZ1WX8',
+    },
+    14: {
+      name: 'رُبَمَا',
+      en_name: 'Rubama',
+      link: 'https://www.youtube.com/watch?v=fww-e31nJ4E',
+    },
+    15: {
+      name: 'الَّذِي سُبْحَانَ',
+      en_name: 'Subhanallazi',
+      link: 'https://www.youtube.com/watch?v=f2-lBavWOw0',
+    },
+    16: {
+      name: 'أَلَمْ قَالَ',
+      en_name: 'Qal Alam',
+      link: 'https://www.youtube.com/watch?v=DzAqvIgx6Gw',
+    },
+    17: {
+      name: 'اقْتَرَبَ',
+      en_name: 'Aqtarabo',
+      link: 'https://www.youtube.com/watch?v=jJFHtldtjPg',
+    },
+    18: {
+      name: 'أَفْلَحَ قَدْ',
+      en_name: 'Qadd Aflaha',
+      link: 'https://www.youtube.com/watch?v=Qncg4VGEpCk',
+    },
+    19: {
+      name: 'الَّذِينَ وَقَالَ',
+      en_name: 'Wa Qalallazina',
+      link: 'https://www.youtube.com/watch?v=UxUw4jkGNTo',
+    },
+    20: {
+      name: 'خَلَقَ أَمَّنْ',
+      en_name: 'A\'man Khalaq',
+      link: 'https://www.youtube.com/watch?v=pixEVk4jRMg',
+    },
+    21: {
+      name: 'أُوحِيَ مَا اتْلُ',
+      en_name: 'Utlu Ma Oohi',
+      link: 'https://www.youtube.com/watch?v=K-gQZFyzn-4',
+    },
+    22: {
+      name: 'يَقْنُتْ وَمَنْ',
+      en_name: 'Wa Manyaqnut',
+      link: 'https://www.youtube.com/watch?v=VSnyfACLr6Q',
+    },
+    23: {
+      name: 'لِيَ وَمَا',
+      en_name: 'Wa Mali',
+      link: 'https://www.youtube.com/watch?v=cz8ch1vz88M',
+    },
+    24: {
+      name: 'أَظْلَمُ فَمَنْ',
+      en_name: 'Faman Azlam',
+      link: 'https://www.youtube.com/watch?v=M0teuzQMtZU',
+    },
+    25: {
+      name: 'يُرَدُّ إِلَيْهِ',
+      en_name: 'Elahe Yuruddo',
+      link: 'https://www.youtube.com/watch?v=PkRW7GQQZbo',
+    },
+    26: {
+      name: 'حم',
+      en_name: 'Ha\'a Meem',
+      link: 'https://www.youtube.com/watch?v=i0DbF1YFC3Y',
+    },
+    27: {
+      name: 'خَطْبُكُمْ فَمَا قَال',
+      en_name: 'Qala Fama Khatbukum',
+      link: 'https://www.youtube.com/watch?v=vskr-Qd_oyo',
+    },
+    28: {
+      name: 'اللَّهُ سَمِعَ قَدْ',
+      en_name: 'Qadd Sami Allah',
+      link: 'https://www.youtube.com/watch?v=eLSDQR2DjVw',
+    },
+    29: {
+      name: 'الَّذِي تَبَارَكَ',
+      en_name: 'Tabarakallazi',
+      link: 'https://www.youtube.com/watch?v=V3ZGkrTxvsg',
+    },
+    30: {
+      name: 'يَتَسَاءَلُونَ عَمَّ',
+      en_name: 'Amma Yatasa\'aloon',
+      link: 'https://www.youtube.com/watch?v=DI10fWoPN3o',
+    },
+  };
+  
